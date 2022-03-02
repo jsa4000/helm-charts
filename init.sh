079819d9ea38cc53b7251837722bf7cac91f6887
Javier Santos Andrés <jsa4000@gmail.com>
2022-03-02 15:15:18 +0100

    Fixed init script

--- init.sh
+++ init.sh
@@ -8,9 +8,7 @@ cd certs;./create_secrets.sh;cd ..
 
 # Install ArgoCD using helm chart
 echo Installing ArgoCD from Helm Chart using 'argocd-values.yaml'
-helm install argocd -n argocd --create-namespace argo/argo-cd --version 3.33.5 \
-  -f argocd-values.yaml \ 
-  --wait
+helm install argocd -n argocd --create-namespace argo/argo-cd --version 3.33.5 -f argocd-values.yaml --wait
 
 # Create ArgoCD project and Root App (App of Apps)
 kubectl apply -f project.yaml 
